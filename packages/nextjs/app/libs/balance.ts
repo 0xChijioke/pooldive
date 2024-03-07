69e7a6d7327bcc6cefe082b9ed02dda6863c840b
EngrGord <chijokeogbuanya@gmail.com>
2024-03-07 11:17:05 +0100

    Added libs to handle user balances.

---
+++ packages/nextjs/app/libs/balance.ts
@@ -0,0 +1,28 @@
+import { Currency } from '@uniswap/sdk-core'
+import { ethers } from 'ethers'
+import { ERC20_ABI } from './constants'
+import { toReadableAmount } from './conversion'
+
+export async function getCurrencyBalance(
+  provider: ethers.providers.Provider,
+  address: string,
+  currency: Currency
+): Promise<string> {
+  // Handle ETH directly
+  if (currency.isNative) {
+    return ethers.utils.formatEther(await provider.getBalance(address))
+  }
+
+  // Get currency otherwise
+  const currencyContract = new ethers.Contract(
+    currency.address,
+    ERC20_ABI,
+    provider
+  )
+  const balance: number = await currencyContract.balanceOf(address)
+  const decimals: number = await currencyContract.decimals()
+
+  // console.log(address, balance)
+  // Format with proper units (approximate)
+  return toReadableAmount(balance, decimals).toString()
+}
